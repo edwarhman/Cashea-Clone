0dc05c18d608a9c97c5b5bb1305389dd543aa04d
Emerson Warhman <edwarhman@gmail.com>
2024-06-29 13:07:25 -0400

    add react-native-paper library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,22 @@
 import Header from "@/components/Header";
+import { ThemedView } from "@/components/ThemedView";
 import { Slot } from "expo-router";
+import { StyleSheet } from "react-native";
+import { PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
   return (
-    <>
-      <Header />
-      <Slot />
-    </>
+    <PaperProvider>
+      <ThemedView style={styles.container}>
+        <Header />
+        <Slot />
+      </ThemedView>
+    </PaperProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
